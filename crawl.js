97ae6bedc5a0c408b4de7d5e35958fed61c08793
aduatgit <justinszigethy@aol.de>
2023-10-13 14:29:35 +0200

    added crawlpage recursive functionality and report.js for future implementation

--- crawl.js
+++ crawl.js
@@ -1,35 +1,65 @@
 const { JSDOM } = require('jsdom')
 
 
-async function crawlPage(currentURL){
+async function crawlPage(baseURL, currentURL, pages){
     // fetch and parse the html of the currentURL
+    const baseObj = new URL(baseURL);
+    const currObj = new URL(currentURL);
+    if (baseObj.hostname !== currObj.hostname) {
+        return pages
+    }
+
+    const currNorm = normalizeURL(currentURL)
+
+    if (pages[currNorm] > 0) {
+        pages[currNorm]++
+        return pages
+    }
+
+    if (baseURL === currentURL) {
+        pages[currNorm] = 0
+    } else {
+        pages[currNorm] = 1
+    }
+
     console.log(`crawling ${currentURL}`)
+    let htmlBody = ''
+
     try {
       const resp = await fetch(currentURL)
       if (resp.status > 399){
         console.log(`Got HTTP error, status code: ${resp.status}`)
-        return
+        return pages
       }
       const contentType = resp.headers.get('content-type')
       if (!contentType.includes('text/html')){
         console.log(`Got non-html response: ${contentType}`)
-        return
+        return pages
       }
-      console.log(await resp.text())
+      htmlBody = await resp.text()
     } catch (err){
       console.log(err.message)
     }
+
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+    for (const nextURL of nextURLs){
+      pages = await crawlPage(baseURL, nextURL, pages)
+    }
+  
+    return pages
+    
   }
 
-const normalizeURL = (url) => {
-    const oldURL = new URL(url)
-    if (oldURL.pathname.lastIndexOf('/') === oldURL.pathname.length - 1){
-        oldURL.pathname = oldURL.pathname.slice(0, -1)
+  function normalizeURL(url){
+    const urlObj = new URL(url)
+    let fullPath = `${urlObj.host}${urlObj.pathname}`
+    if (fullPath.length > 0 && fullPath.slice(-1) === '/'){
+      fullPath = fullPath.slice(0, -1)
     }
-    return oldURL.hostname + oldURL.pathname
-}
+    return fullPath
+  }
 
-const getURLsFromHTML = (htmlBody, baseURL) => {
+function getURLsFromHTML(htmlBody, baseURL){
     const URLs = []
     const dom = new JSDOM(htmlBody)
     const aElements = dom.window._document.querySelectorAll('a')
